9b77c77f148c99cd505862b9d0a6c4a8ad6342eb
Don Brown <mrdon@twdata.org>
2014-02-16 10:44:07 -0700

    Simplified app

--- app.py
+++ app.py
@@ -1,37 +1,22 @@
-import os
-import asyncio
-from flask import Flask, jsonify
-from flask.ext.ac import create_notifications, create_installable, create_addon
+from flask import jsonify
+from flask.ext.ac import create_addon_app
 
-app = Flask(__name__)
-
-app.config.update({
-    "DEBUG": True if "true" == os.environ.get("DEBUG", "true") else False,
-    "PLUGIN_KEY": os.environ.get("PLUGIN_KEY", "hc-alias"),
-    "ADDON_NAME": os.environ.get("ADDON_NAME", "Test me"),
-    "FROM_NAME": os.environ.get("FROM_NAME", "Test")
-})
-
-
-base_url = "http://192.168.33.1:5000"
-
-@asyncio.coroutine
-def init():
-    yield from create_addon(app)
-    app.register_blueprint(create_installable('installable', allow_global=False), url_prefix="/installable")
-    app.register_blueprint(create_notifications(1))
-asyncio.get_event_loop().run_until_complete(init())
+app = create_addon_app(__name__,
+                       plugin_key="hc-alias",
+                       addon_name="HC Alias",
+                       from_name="Alias",
+                       base_url="http://192.168.33.1:5000")
 
 @app.route('/')
 def capabilities():
     return jsonify({
         "links": {
-            "self":         base_url,
-            "homepage":     base_url
+            "self":         app.config.get("BASE_URL"),
+            "homepage":     app.config.get("BASE_URL")
         },
         "key": app.config.get("PLUGIN_KEY"),
         "name": app.config.get("ADDON_NAME"),
-        "description": "HipChat connect add-on that sends Bitbucket events to a room",
+        "description": "HipChat connect add-on that sends supports aliases for group mention",
         "vendor": {
             "name": "Atlassian Labs",
             "url": "https://atlassian.com"
@@ -40,7 +25,7 @@ def capabilities():
             "installable": {
                 "allowGlobal": False,
                 "allowRoom": True,
-                "callbackUrl": base_url + "/installable/"
+                "callbackUrl": app.config.get("BASE_URL") + "/installable/"
             },
             "hipchatApiConsumer": {
                 "scopes": [
